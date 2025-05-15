dbf408ab22e20eb19804b702c004fd74297dde7d
petrenko@binaryanvil.com <mpetrenko@binaryanvil.com>
2025-05-15 14:11:28 +0300

    hot fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,9 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import Job from '../models/jobModel.js';
+import { formatImage } from '../middleware/multerMiddleware.js';
 
 import cloudinary from 'cloudinary';
-import { promises as fs } from 'fs'; // file system
 
 export const getCurrentUser = async (req, res) => {
 	const user = await User.findOne({ _id: req.user.userId });
@@ -23,7 +23,7 @@ export const updateUser = async (req, res) => {
 
 	if (req.file) {
 		const response = await cloudinary.v2.uploader.upload(req.file.path);
-		await fs.unlink(req.file.path);
+
 		newUser.avatar = response.secure_url;
 		newUser.avatarPublicId = response.public_id;
 	}
